2c72752614feaa107f1c552064bbc725b55b22c3
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2023-04-05 17:50:11 +0200

    added filter in basket queryset

--- users/views.py
+++ users/views.py
@@ -36,7 +36,7 @@ class ProfileView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['basket'] = Basket.objects.all()
+        context['basket'] = Basket.objects.filter(user=self.request.user)
         return context
 
 
